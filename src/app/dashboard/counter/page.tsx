8341d0dfeb556ae591ebe9a61cb425ee9726222f
Eric Nova <ericnova23@hotmail.com>
2023-10-13 23:41:40 -0500

    Section 4 Video 9/13

---
+++ src/app/dashboard/counter/page.tsx
@@ -0,0 +1,13 @@
+
+export default function CounterPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      
+        <h2 className={`mb-3 text-2xl font-semibold`}>
+          Hola Counter
+        </h2>
+    </div>
+  </main>
+  );
+}
